0873d9f48eee140436cfdc0502b715694329dba3
Stewart Adam (MSFT) <stad@microsoft.com>
2025-12-02 17:01:58 -0800

    feat(devcontainer): improve performance and reliability of devcontainer launch

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -5,10 +5,45 @@
 
 set -euo pipefail
 
-main() {
+# Volume ownership is not set automatically due to a bug:
+# https://github.com/microsoft/vscode-remote-release/issues/9931
+#
+# IMPORTANT: workaround requires Docker base image to have password-less sudo.
+function fix_volume_ownership() {
+  volume_path="$1"
+
+  if [ ! -d "$volume_path" ]; then
+    echo "ERROR: the volume path provided '$volume_path' does not exist."
+    exit 1
+  fi
+
+  echo "Setting volume ownership for $volume_path"
+  sudo chown $USER:$USER "$volume_path"
+}
+
+function fix_volume_ownerships() {
+  echo "Applying volume ownership workaround (see microsoft/vscode-remote-release#9931)..."
+  fix_volume_ownership "/home/$USER/.config"
+  fix_volume_ownership "/workspace/node_modules"
+}
+
+function npm_install() {
   echo "Installing NPM dependencies..."
   npm install
   echo "NPM dependencies installed successfully"
+}
+
+# Adds a root CA to the system certificate store. Useful if developer machines
+# have MITM TLS inspection happening, e.g. with ZScaler.
+if [ -f .devcontainer/*.crt ]; then
+  sudo cp .devcontainer/*.crt /usr/local/share/ca-certificates/
+  sudo update-ca-certificates
+fi
+
+main() {
+  fix_volume_ownerships
+  npm_install
+  update_ca_certs
 }
 
 main "$@"
